ae226e5984c3e0dd4c7d0574982901e949a58dde
tejasghate02 <ghatetejas8@gmail.com>
2025-04-27 14:27:44 +0530

    add deleteApi call

--- tests/delete.spec.ts
+++ tests/delete.spec.ts
@@ -1,4 +1,9 @@
-//perform one delete caal and asset status code
+//perform one delete call and asset status code
+import {test, expect} from '@playwright/test'
+
+test('DELETE request using request context', async()=>{
+
+})
 
 //then perform get call and check the status code it should be 404
 const getReq = await request.get();
